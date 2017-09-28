6737e4e3c7a97f6bb430a5d7ea7cb0628be965a8
elespdn <spadinielena@gmail.com>
2017-09-28 17:12:07 +0200

    test

--- js/imagerandom.js
+++
@@ -1,8 +0,0 @@
-
-
-function choosePic() {
-	var num = Math.floor(Math.random() * 306 + 1); 
-	imgrandom = "../site_img/" + num +".jpg";
-	document.getElementsByClassName("imagerandom")[0].src=imgrandom;   
-				// [0] così si prende il primo, perché getElementSSSSSbyClass !
-}
