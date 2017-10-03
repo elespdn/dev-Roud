e4748a7190c93112b8c88b7dac7a2a9ad4a72d7f
elespdn <spadinielena@gmail.com>
2017-10-03 18:16:29 +0200

    in update still: dropdown and checkbox prefill ok

--- php/script_update.php
+++ php/script_update.php
@@ -17,7 +17,7 @@ $record_id = $_POST['record_id'];
 
 
 
-$select2prefill = "SELECT fiche_texte.id, titre, cote, nouvelle_cote, ensemble_id, photocopy, type.type, annotation, addition, support.support, numbered, support_info, instrument.instrument, color.color, other_tool, statut.statut, genre.genre, dates, dossier.dossier, dossierplus, publie, numerise, numerise_info, commentaire, resp.resp FROM fiche_texte INNER JOIN ensemble ON fiche_texte.ensemble_id = ensemble.id INNER JOIN type ON fiche_texte.type_id = type.id INNER JOIN support ON fiche_texte.support_id = support.id INNER JOIN instrument ON fiche_texte.instrument_id = instrument.id INNER JOIN color ON fiche_texte.color_id = color.id INNER JOIN statut ON fiche_texte.statut_id = statut.id INNER JOIN genre ON fiche_texte.genre_id = genre.id LEFT JOIN dossier ON fiche_texte.dossier_id = dossier.id LEFT JOIN resp ON fiche_texte.resp_id = resp.id WHERE fiche_texte.id = '$record_id' ";
+$select2prefill = "SELECT fiche_texte.id, titre, cote, nouvelle_cote, ensemble_id, photocopy, type_id, annotation, addition, support_id, numbered, support_info, instrument_id, color_id, other_tool, statut_id, genre_id, dates, dossier_id, dossierplus, publie, numerise, numerise_info, commentaire, resp_id FROM fiche_texte WHERE fiche_texte.id = '$record_id' ";
 
 $query = mysqli_query($con, $select2prefill) or die ("impossible de VISUALISER Les données");  
 while ($row = mysqli_fetch_array($query)) 
@@ -29,28 +29,28 @@ while ($row = mysqli_fetch_array($query))
 		$nouvelle_cote_record_id = $row['nouvelle_cote'];
 		$ensemble_record_id = $row['ensemble_id'];
 		$photocopy_record_id = $row['photocopy'];
-		$type_record_id = $row['type'];
+		$type_record_id = $row['type_id'];
 		$annotation_record_id = $row['annotation'];
 		$addition_record_id = $row['addition'];
-		$support_record_id = $row['support'];
+		$support_record_id = $row['support_id'];
 		$numbered_record_id = $row['numbered'];
 		$support_info_record_id = $row['support_info'];
-		$instrument_record_id = $row['instrument'];
-		$color_record_id = $row['color'];
+		$instrument_record_id = $row['instrument_id'];
+		$color_record_id = $row['color_id'];
 		$other_tool_record_id = $row['other_tool'];
 		$dates_record_id = $row['dates'];
-		$genre_record_id = $row['genre'];
-		$dossier_record_id = $row['dossier'];
+		$genre_record_id = $row['genre_id'];
+		$dossier_record_id = $row['dossier_id'];
 		$dossierplus_record_id = $row['dossierplus'];
-		$statut_record_id = $row['statut'];
+		$statut_record_id = $row['statut_id'];
 		$publie_record_id = $row['publie'];
 		$numerise_record_id = $row['numerise'];
 		$numerise_info_record_id = $row['numerise_info'];
 		$commentaire_record_id = $row['commentaire'];
-		$resp_record_id = $row['resp'];
+		$resp_record_id = $row['resp_id'];
 	}
 
-echo $record_id;
+// %%%%%%%%% echo table same as form_insert.php, plus variables from the results of the query $select2prefill above
 
 echo "		<table class='table_insert'>
 				<tr>
@@ -82,7 +82,7 @@ echo "		<table class='table_insert'>
 $sql = "SELECT * FROM ensemble";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{	
+	{	
 	// if the ID from the array is the same to $ensemble_record_id (from query $select2prefill)
 	if ($row['id']==$ensemble_record_id) {  		
 
@@ -97,12 +97,7 @@ while ($row = mysqli_fetch_array($query))
 
         // normal array, plus the variable $selected, which gives a different output following what defined above
         echo "<option value='". $row['id']."' ". $selected .">".$row['ensemble']. '</option>'; 
-
-
-
-
-// echo "<option value='". $row['id']."'>".$row['ensemble']. '</option>';
-} 
+	} 
 
 echo "</select></td>
 				</tr> 
@@ -117,8 +112,16 @@ echo "</select></td>
 				
 				<tr>
 					<td>
-						<legend>Photocopie </legend>
-						<input type='checkbox' name='photocopy' value='oui'>
+						<legend>Photocopie</legend>";
+						if ($photocopy_record_id=='oui') { 
+            				$checked = 'checked';  
+					        }
+					    else {		
+					            $checked = '';			
+					    }
+					    echo "<input type='checkbox' name='photocopy' value='oui' ". $checked .">"; 
+
+				echo "	
 					</td>
 				</tr>
 
@@ -132,22 +135,43 @@ echo "</select></td>
 $sql = "SELECT * FROM type";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['type']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$type_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['type']. '</option>'; 
+	} 
 
 echo "					</select>
 					</td>
 					
 					<td>
-						<legend>Annoté </legend>
-						<input type='checkbox' name='annotation' value='oui'>			
-					</td>
+						<legend>Annoté </legend>";
+						if ($annotation_record_id=='oui') { 
+            				$checked = 'checked';  
+					        }
+					    else {		
+					            $checked = '';			
+					    }
+					    echo "<input type='checkbox' name='annotation' value='oui' ". $checked .">";
+
+			echo "	</td>
 
 				
 					<td><legend>Avec adjonction(s) </legend>
-						<input type='checkbox' name='addition' value='oui'>
-					</td>
+						<legend>Annoté </legend>";
+						if ($addition_record_id=='oui') { 
+            				$checked = 'checked';  
+					        }
+					    else {		
+					            $checked = '';			
+					    }
+					    echo "<input type='checkbox' name='addition' value='oui' ". $checked .">";
+			echo "	</td>
 
 				</tr>    
 
@@ -159,22 +183,37 @@ echo "					</select>
 $sql = "SELECT * FROM support";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['support']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$support_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['support']. '</option>'; 
+	} 
+ 
 
 echo "					</select>
 					</td>
 
 					<td>
-						<legend>Numeroté </legend>
-						<input type='checkbox' name='numbered' value='oui'>
-					</td>
+						<legend>Numeroté </legend>";
+						if ($numbered_record_id=='oui') { 
+            				$checked = 'checked';  
+					        }
+					    else {		
+					            $checked = '';			
+					    }
+					    echo "<input type='checkbox' name='numbered' value='oui' ". $checked .">";
+
+			echo "	</td>
 				</tr> 
 				<tr>
 					<td><legend>Plus d'info sur le support </legend></td>
 					<td colspan='2'>
-						<textarea rows='3' cols='50' name='support_info'></textarea> 
+						<textarea rows='3' cols='50' name='support_info'>". $support_info_record_id ."</textarea> 
 					</td>
 					<td>
 						<span class='suggest'>Suggestions: étendue, quantité de feuillets, recto/verso, taille du cahier, papier particulier (bleu, pelure, de qualité, enveloppe), couleur des pages (si pas blanc), couverture.</span>
@@ -190,9 +229,16 @@ echo "					</select>
 $sql = "SELECT * FROM instrument";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['instrument']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$instrument_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['instrument']. '</option>'; 
+	} 
 
 echo "					</select>
 					&emsp;&emsp;&emsp;&emsp;&emsp;
@@ -203,9 +249,16 @@ echo "					</select>
 $sql = "SELECT * FROM color";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['color']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$color_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['color']. '</option>'; 
+	} 
 
 echo "					</select>
 					</td>
@@ -214,7 +267,7 @@ echo "					</select>
 				<tr>
 					<td><legend>Autre(s) instrument(s) d'écriture </legend></td>
 					<td colspan='2'>
-						<textarea rows='3' cols='50' name='other_tool'></textarea> 
+						<textarea rows='3' cols='50' name='other_tool'>". $other_tool_record_id ."</textarea> 
 					</td>
 					<td>
 						<span class='suggest'>Suggestions : spécifier si l'instrument a été utilisé pour des annotations ou adjonctions ; utiliser les mêmes termes que dans l'onglet au-dessus (crayon, machine à écrire, etc.).</span>
@@ -236,7 +289,7 @@ echo "					</select>
 					<td><legend>Date / Datation </legend></td>
 				<!-- Aggiungere una casellina per decidere se è Date o Datation, e constraints per inserire la data nel modo giusto con js! -->
 					<td>
-						<input type='text' id='date' name='date' pattern='^\d\d\d\d(.)*|^\?(.)*'></input>
+						<input type='text' id='date' name='date' pattern='^\d\d\d\d(.)*|^\?(.)*' value='". $dates_record_id ."'></input>
 					</td>
 					<td>
 						<span class='suggest'>S'il s'agit d'une datation, ajouter un ast&#233;risque apr&#232;s une espace, par exemple '1956 *', '1945-06 *'.</span>
@@ -252,9 +305,16 @@ echo "					</select>
 $sql = "SELECT * FROM genre";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['genre']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$genre_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['genre']. '</option>'; 
+	} 
 
 
 echo "</select></td>
@@ -269,13 +329,20 @@ echo "</select></td>
 $sql = "SELECT * FROM dossier";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['dossier']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$dossier_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['dossier']. '</option>'; 
+	} 
 
 echo "</select></td>
 					<td>
-						<textarea rows='2' cols='50' name='dossierplus'></textarea>
+						<textarea rows='2' cols='50' name='dossierplus'>". $dossierplus_record_id ."</textarea>
 					</td>
 				</tr>
 
@@ -288,9 +355,16 @@ echo "</select></td>
 $sql = "SELECT * FROM statut";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['statut']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$statut_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['statut']. '</option>'; 
+	} 
 
 
 echo "	</select></td>
@@ -301,7 +375,7 @@ echo "	</select></td>
 					
 					
 					<td>
-						<textarea rows='3' cols='30' name='publie'></textarea> 
+						<textarea rows='3' cols='30' name='publie'>". $publie_record_id ."</textarea> 
 					</td>
 				</tr>   
 
@@ -315,7 +389,7 @@ echo "	</select></td>
 
 			<table class='table_insert'>
 				<tr>
-					<td><legend>Numeriser </legend></td>
+					<td><legend>Numeriser</legend></td>
 					<td><select name='digitize'>
 							<option value='oui'>Oui</option>
 							<option value='non'>Non</option>
@@ -328,7 +402,7 @@ echo "	</select></td>
 						<legend>Notes pour le site </legend>
 					</td>
 					<td>
-						<textarea rows='4' cols='50' name='numerise_info'></textarea>
+						<textarea rows='4' cols='50' name='numerise_info'>". $numerise_info_record_id ."</textarea>
 					</td>
 				<tr/>
 			</table>
@@ -341,7 +415,7 @@ echo "	</select></td>
 				<tr>
 					<td><legend>Commentaire </legend></td> 
 					<td>
-						<textarea rows='5' cols='50' name='comment'></textarea> 
+						<textarea rows='5' cols='50' name='comment'>". $commentaire_record_id ."</textarea> 
 					</td>
 				</tr>
 
@@ -356,9 +430,16 @@ echo "	</select></td>
 $sql = "SELECT * FROM resp";
 $query = mysqli_query($con, $sql) or die ("impossible de sélectionner des données");  
 while ($row = mysqli_fetch_array($query)) 
-{  
-echo "<option value='". $row['id']."'>".$row['resp']. '</option>';
-} 
+	{	
+	// see ensemble for more comments explaining this if
+	if ($row['id']==$resp_record_id) { 
+            $selected = 'selected="selected"';  
+        }
+        else {		
+            $selected = '';			
+        }
+        echo "<option value='". $row['id']."' ". $selected .">".$row['resp']. '</option>'; 
+	} 
 
 
 echo "					</select>
